e78463119f7febf66609af1a358d97defcb473fa
sylwia.rutkiewicz <sylwia.rutkiewicz@intive.com>
2018-11-26 12:15:36 +0100

    First commit

---
+++ src/test/java/basicweb/JUnitIntorductionTest.java
@@ -0,0 +1,19 @@
+package basicweb;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.*;
+
+public class JUnitIntorductionTest {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
