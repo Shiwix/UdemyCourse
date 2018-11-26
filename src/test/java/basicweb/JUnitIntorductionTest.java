ad933849fac0d789c13d115bca46e516655300cd
sylwia.rutkiewicz <sylwia.rutkiewicz@intive.com>
2018-11-26 12:51:19 +0100

    Second commit

--- src/test/java/basicweb/JUnitIntorductionTest.java
+++ src/test/java/basicweb/JUnitIntorductionTest.java
@@ -3,6 +3,10 @@ package basicweb;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +15,7 @@ public class JUnitIntorductionTest {
 
     @Before
     public void setUp() throws Exception {
+        driver = new ChromeDriver();
     }
 
     @After
